0b6db7815a784198fda8d0c29818296cee6c9053
nosh11 <101454424+nosh11@users.noreply.github.com>
2025-04-26 16:51:52 +0900

    remove extras

--- src/editor/widgets/map_painter/__init__.py
+++
@@ -1,4 +0,0 @@
-from mapeditor.widgets.map_painter.components.map_editor_canvas import *
-from mapeditor.widgets.map_painter.chip_cache import SharedImageCache
-from mapeditor.widgets.map_painter.chipset_canvas import ChipSetCanvas
-from mapeditor.widgets.map_painter.components.map_editor_canvas import MapTileCanvas
